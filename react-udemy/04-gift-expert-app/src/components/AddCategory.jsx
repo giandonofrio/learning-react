b732035ab1936834bc6f3efdef926f05e5c334ce
Gianluca D'Onofrio <gianlucadonofrio@MBP-de-Gianluca.fibertel.com.ar>
2022-07-18 21:17:52 -0300

    adding files

---
+++ react-udemy/04-gift-expert-app/src/components/AddCategory.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { useState } from "react";
+
+export const AddCategory = ({ onNewCategory }) => {
+  const [inputValue, setInputValue] = useState([]);
+
+  const onInputChange = ({ target }) => {
+    setInputValue(target.value);
+  };
+  const onSubmit = (e) => {
+    e.preventDefault();
+    if (inputValue.length <= 1) return;
+
+    onNewCategory(inputValue.trim());
+    setInputValue("");
+  };
+
+  return (
+    <form onSubmit={(e) => onSubmit(e)}>
+      <input
+        type="text"
+        placeholder="Buscar Gifts"
+        value={inputValue}
+        onChange={onInputChange}
+      />
+    </form>
+  );
+};
